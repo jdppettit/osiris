b68cb0620e7e1529af034dbb8d21e7a4bf09260c
Joseph Pettit <jpettit@pettitservers.com>
2019-07-01 23:13:58 -0500

    first commit

---
+++ osiris/lib/osiris.ex
@@ -0,0 +1,18 @@
+defmodule Osiris do
+  @moduledoc """
+  Documentation for Osiris.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Osiris.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ osiris/test/osiris_test.exs
@@ -0,0 +1,8 @@
+defmodule OsirisTest do
+  use ExUnit.Case
+  doctest Osiris
+
+  test "greets the world" do
+    assert Osiris.hello() == :world
+  end
+end
